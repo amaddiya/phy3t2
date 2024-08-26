1bbfd61e55f92562473cd8455bfd9eba0faac161
amaddiya <174859382+amaddiya@users.noreply.github.com>
2024-08-26 14:41:54 +0800

    Add files via upload

---
+++ day3T2.py
@@ -0,0 +1,67 @@
+# Number 1
+# fruits = ("apple", "banana", "cherry", "date")
+# print("First:", fruits[0])
+# print("Last:", fruits[-1])
+
+# number 2
+
+# numbers = (1, 2, 3, 4, 5)
+# a, b, *rest = numbers
+# print("a:", a)
+# print("b:", b)
+# print("rest:", rest)
+
+# number 3
+# fruits = ("apple", "banana", "cherry", "date")
+# fruits[1] = "blueberry"
+# This is happened because tuple is immutable hence it cannot be change or altered
+
+# number 4
+# colors = {"red", "green", "blue", "yellow"}
+# colors.add("purple")
+# print(colors)
+
+# # number 5
+# # Create the sets
+# colors = {"red", "green", "blue", "yellow", "purple"}
+# primary = {"red", "blue", "yellow"}
+
+
+# intersection = colors & primary
+# union = colors | primary
+# difference = colors - primary
+
+# print("Intersection:", intersection)
+# print("Union:", union)
+# print("Difference:", difference)
+
+# greenprimary = "green" in colors
+# print('"green" in primary_colors:', greenprimary)
+
+# orangecolors = "orange" not in colors
+# print('"orange" not in colors:', orangecolors)
+
+#task3
+# student_grades = {
+#     "Alice": 85,
+#     "Bob": 90,
+#     "Charlie": 78,
+#     "Diana": 92
+# }
+# print("Bob's grade:", student_grades["Bob"])
+
+# student_grades["Eve"] = 88
+# student_grades.update({"Alice": 95})
+# del student_grades["Charlie"]
+# print("Updated student grades:", student_grades)
+
+
+# student_grades = {
+#     "Alice": 95,
+#     "Bob": 90,
+#     "Diana": 92,
+#     "Eve": 88
+# }
+
+# for student, grade in student_grades.items():
+#     print(f"Student: {student}, Grade: {grade}")
